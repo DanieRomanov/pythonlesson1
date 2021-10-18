537fcb673c75cef34741f0172959e5d28ce1d83e
DanieRomanov <92585764+DanieRomanov@users.noreply.github.com>
2021-10-18 20:12:46 +0300

    Delete task1.py

--- task1.py
+++
@@ -1,9 +0,0 @@
-num = int(input('Введите трёхзначное число'))
-a = num // 100
-b = (num % 100) // 10
-с = num % 10
-sum = a + b + с
-multiply = a * b * с
-
-print(f'сумма цифр: {sum}\n'
-      f'произведение цифр: {multiply}')
